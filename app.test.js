0471eaee7f1a701c2a24c784d68dd8129ac65e61
Krys <rockwithtrickz@gmail.com>
2022-05-19 13:33:40 +0200

    getting todo id and post id
    added test for getting todo ids and post ids

--- app.test.js
+++ app.test.js
@@ -11,6 +11,7 @@ describe("ToDo test", () => {
         expect(response.body).toEqual(
           expect.arrayContaining([
             expect.objectContaining({
+              id: expect.any(Number),
               name: expect.any(String),
               completed: expect.any(Boolean),
             }),
@@ -18,46 +19,49 @@ describe("ToDo test", () => {
         );
       });
   });
+
   it("GET /todos/id --> get specific todo by id", () => {
     return request(app)
       .get("/todos/1")
-      .expect("Content-Type", /json/)
       .expect(200)
+      .expect("Content-Type", /json/)
+
       .then((response) => {
         expect(response.body).toEqual(
-          expect.arrayContaining([
-            expect.objectContaining({
-              name: expect.any(String),
-              completed: expect.any(Boolean),
-            }),
-          ])
+          expect.objectContaining({
+            id: expect.any(Number),
+            name: expect.any(String),
+            completed: expect.any(Boolean),
+          })
         );
       });
   });
 
   it("GET /todos/id --> return 404 if id hasn't been found", () => {
-    return request(app).get("/todos/999").expect(404);
+    return request(app).get("/todos/9999").expect(404);
   });
 
-  it("GET /todos --> validate body request", () => {
-    return request(app).post("/todos/").send({ name: 1234556 }).expect(422);
+  it("POST /todos --> validate body request", () => {
+    return request(app).post("/todos").send({ name: 123 }).expect(422);
   });
 
-  it("POST /todos --> created todos", () => {
-    return request(app)
-      .post("/todos/")
-      .send({ name: "do something" })
-      .expect("Content-Type", /json/)
-      .expect(201)
-      .then((response) => {
-        expect(response.body).toEqual(
-          expect.arrayContaining([
+  it("POST /todos --> created todo", () => {
+    return (
+      request(app)
+        .post("/todos")
+        .send({ name: "do something" })
+        .expect(201)
+        .expect("Content-Type", /json/)
+        // .expect((response) => { console.log(response); }) you can log response to check error
+        .then((response) => {
+          expect(response.body).toEqual(
             expect.objectContaining({
-              name: expect.any(String),
-              completed: expect.any(Boolean),
-            }),
-          ])
-        );
-      });
+              id: expect.any(Number),
+              name: "do something",
+              completed: false,
+            })
+          );
+        })
+    );
   });
 });
--- routes/todos.js
+++ routes/todos.js
@@ -1,11 +1,36 @@
 var express = require("express");
 var router = express.Router();
+var createError = require("http-errors");
 
-const todos = [{ id: 1, name: "do something cool", completed: false }];
+const todos = [{ id: 1, name: "do something", completed: false }];
 
 /* GET todos. */
 router.get("/", function (req, res, next) {
   res.json(todos);
+});
+router.get("/:id", function (req, res, next) {
+  const findTodo = todos.find((todo) => todo.id === Number(req.params.id));
+  if (!findTodo) {
+    return next(createError(404, "id not found"));
+  }
+  res.json(findTodo);
+});
+
+router.post("/", function (req, res, next) {
+  const { body } = req;
+
+  if (typeof body.name !== "string") {
+    return next(createError(422, "Validation error"));
+  }
+
+  const newTodo = {
+    id: todos.length + 1,
+    name: body.name,
+    completed: false,
+  };
+
+  todos.push(newTodo);
+  res.status(201).json(newTodo);
 });
 
 module.exports = router;
